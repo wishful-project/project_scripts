e40540b1af1d1d0a9835a3868687b7925644c10b
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-09 17:33:43 +0100

    update

--- install_wishful.sh
+++ install_wishful.sh
@@ -3,23 +3,6 @@
 echo "Install required libraries ..."
 sudo apt-get install wget git python python-virtualenv python-dev
 
-echo "Get repo ..."
-wget https://storage.googleapis.com/git-repo-downloads/repo
-
-chmod a+x ./repo
-
-echo "Init repo ..."
-./repo init -u ssh://git@gitlab.tubit.tu-berlin.de/wishful/wishful_manifests.git
-
-echo "Repo sync ..."
-./repo sync
-
-echo "Switch to master ..."
-./repo start master --all
-
-echo "Check status of all repositories ..."
-./repo status
-
 echo "Create virtual environment ..."
 virtualenv dev
 
